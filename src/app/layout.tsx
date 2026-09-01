f930cb24cd978aed3a8f48f07ce45b49d8f912c0
Serg Rogov <serg.rogov@gmail.com>
2026-09-01 19:24:04 -0300

    added worked to PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SerwistProvider } from "@serwist/turbopack/react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { NavigationTracker } from "@/components/navigation-tracker";
@@ -49,12 +50,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-gray-50">
-        <NavigationTracker />
-        <NotificationHost />
-        <Header />
-        {children}
-        <Footer />
-        <FloatingLikesButton />
+        <SerwistProvider swUrl="/serwist/sw.js">
+          <NavigationTracker />
+          <NotificationHost />
+          <Header />
+          {children}
+          <Footer />
+          <FloatingLikesButton />
+        </SerwistProvider>
       </body>
     </html>
   );
