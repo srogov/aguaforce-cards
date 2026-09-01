83e5ffcb202ada6109df15aca506167d8f4d9931
Serg Rogov <serg.rogov@gmail.com>
2026-09-01 11:26:01 -0300

    updated external links

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const LINKS = {
     home: (source: string) => `/?${new URLSearchParams({ utm_source: source, utm_medium: 'exercise_library' })}`,
     shop: 'https://www.aguaforce.com',
-    terms: 'https://www.aguaforce.com/terms',
-    privacy: 'https://www.aguaforce.com/privacy',
-    contact: 'https://www.aguaforce.com/contact',
+    terms: 'https://aguaforce.com/pages/terms-of-service',
+    privacy: 'https://aguaforce.com/pages/privacy-policy',
+    contact: 'https://aguaforce.com/pages/contact',
     facebook: 'https://www.facebook.com/aguaforceweights',
     instagram: 'https://www.instagram.com/aguaforce.weights/',
     youtube: 'https://www.youtube.com/@aguaforceweights',
